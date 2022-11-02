79e14c7fe6bff4c8be1352cc4d1f6f94b08f6ffd
Wilton <dev3@evop.com.br>
2022-11-02 11:56:50 -0400

    montar precompiler

--- loggibud/v1/baselines/task2/dsnr.py
+++ loggibud/v1/baselines/task2/dsnr.py
@@ -25,6 +25,9 @@ from loggibud.v1.types import (
 
 logger = logging.getLogger(__name__)
 
+class NoRouterFound(Exception):
+    def __init__(self, message):
+        super().__init__(message)
 @dataclass
 class DynamicSearchNeighbourRoutes:
     batch_size: int = 75
@@ -94,7 +97,7 @@ def orderBatch(batch, instance, vehiclesPossibles, order, md):
         orderBatch = buildMetric(instance, batch, packetsExist, order, md)
     else:
         orderBatch = batch
-    return 
+    return orderBatch
 
 def whoIsOrder(vehiclesPossibles, instance):
     deposit = instance.origin
@@ -113,7 +116,6 @@ def buildMetric(instance, batch, packetsExist, order, md):
     orderBatchList = []
     for p in batch:
         distances_pack[p.idu] = meanDistance(p, packetsExist, md)
-    # construir o batch crescente/decrescente
     for i in sorted(distances_pack, key = distances_pack.get, reverse=order):
         orderBatchList.append(instance.deliveries[i])
     orderBatch = deque(orderBatchList)
@@ -128,8 +130,198 @@ def meanDistance(p, packetsExist, md):
             d += md[p.idu+1][atual.idu+1]
     return d/len(packetsExist)
 
-def routePackage():
-    d = 2
+def routePackage(
+    vPoss, 
+    packet: Delivery, 
+    md, 
+    instance, 
+    batch, 
+    k_near, 
+    deliveries, 
+    loop
+    ):
+    """
+    Roteirizando um pacote dinamico (packet)
+    """
+    attempt = 0
+    id_pack = packet.idu
+    routes_neig = lookForNeighboringRoutes(packet, deliveries, md, vPoss, k_near) 
+    if loop == False:
+        for rSelect in routes_neig:
+            try:
+                insertionPackInRoute(
+                    instance, id_pack, vPoss, rSelect, batch, md, deliveries
+                ) 
+                break
+            except NoRouterFound:
+                attempt += 1
+    if attempt == len(routes_neig) or loop: 
+        newRoute, id_route1 = createNewRoute(id_pack, vPoss, 180, deliveries) 
+        if attempt != 0:
+            rneigh = lookForNeighboringRoutes(packet, deliveries, md, vPoss, 1) 
+            r1, r2 = bestLocalTwoOptModificated(
+                newRoute, vPoss[rneigh[0]][0], md, instance
+            )
+            createNewSolution(id_route1, rneigh[0], r1, r2, vPoss)
+
+def insertionPackInRoute(instance: CVRPInstance, id_packet: int, vPoss,
+    rSelect, batch_d, md, deliveries):
+    """
+    Tenta inserir um pacote dinamico na rota, 
+    se falhar tenta expulsar o pior pacote da rota selecionada
+    """
+    route_fake = insertNewPacket(id_packet, vPoss[rSelect][0], md) 
+    if capacityRoute(route_fake, instance, vPoss[rSelect][1]): 
+        deliveries.append(id_packet)
+        vPoss[rSelect][0] = route_fake
+    else:
+        worst_pack_id = selectWorstPacket(vPoss[rSelect][0], md)
+        route_worstpack = createRouteNoWorst(worst_pack_id, vPoss[rSelect][0])
+        route_newpacket = insertNewPacket(id_packet, route_worstpack, md)
+        if compareRoutes(route_newpacket, vPoss[rSelect][0], md):
+            vPoss[rSelect][0] = kickOutWorst(
+                instance, vPoss[rSelect][0], worst_pack_id, batch_d, deliveries
+            )
+            insertionPackInRoute(instance, id_packet, vPoss, rSelect, batch_d, md, deliveries)
+        else:
+            raise NoRouterFound("Próxima rota!")
+
+def kickOutWorst(instance, route, worst_pack_id, batch_d, deliveries):
+    """
+    Acrescenta o pior pacote na lista dos não visitados
+    e retira o pior pacote da rota original
+    """
+    batch_d.append(instance.deliveries[worst_pack_id])
+    route = createRouteNoWorst(worst_pack_id, route)  
+    deliveries.remove(worst_pack_id)
+    return route
+
+def compareRoutes(route1, route2, md):
+    """
+    Compara duas rotas e devolve 
+    Verdadeiro se a primeira for melhor e 
+    False se a segunda for melhor
+    """
+    score1 = computeDistanceRoute(route1, md)
+    score2 = computeDistanceRoute(route2, md)
+    if score1 < score2:
+        return True
+    else:
+        return False
+
+def computeDistanceRoute(route, matrix_distance):
+  """Retorna a distancia percorrida pela rota"""
+  distance = 0
+  for o in range(len(route)-1):
+    d = o + 1
+    distance += matrix_distance[route[o]+1][route[d]+1]
+  return distance
+
+def createRouteNoWorst(id_pack, route):
+    """
+    Cria uma lista sem o id_pack da rota
+    """
+    if id_pack == -1:
+        return route.copy()
+    return [id for id in route if id != id_pack]
+
+def capacityRoute(route, instance: CVRPInstance, capacityMax) -> bool:
+    """Retorna verdadeiro se a capacidade da rota foi respeitada"""
+    cap = 0
+    for id_pack in route:
+        cap += instance.deliveries[id_pack].size
+    return cap <= capacityMax
+
+def createNewSolution(id_route1, id_route2, r1, r2, vehiclesPossibles):
+    """a nova solução deve compor o vehiclePossible"""
+    vehiclesPossibles[id_route1] = [r1,vehiclesPossibles[id_route1][1]]
+    vehiclesPossibles[id_route2] = [r2,vehiclesPossibles[id_route2][1]]
+
+def insertNewPacket(id_packet, route, matrix_distance):
+    """Insere o pacote dinamico na melhor posição da rota"""
+    route_supos = route.copy()
+    p_insertion = []
+    for i in range(1,len(route)+1):
+        route_aux = [el for el in route]
+        route_aux.insert(i, id_packet)
+        p_insertion.append(route_aux)
+    scores = []
+    for possible in p_insertion:
+        score = calculateDiferenceDistanceRoute(matrix_distance, route, possible)
+        scores.append(score)
+    route_supos = p_insertion[scores.index(min(scores, key = float))] 
+    return route_supos    
+
+def calculateDiferenceDistanceRoute(
+    matrix_distance, 
+    old_possible,
+    possible # lista com pacotes de como sera atendido
+    ):
+  """Calcular a diferença entre uma rota antiga por uma rota nova"""
+  distanceOld = 0 
+  distanceNew = 0 
+  for old in range(len(old_possible)-1):
+    dest = old + 1
+    distanceOld += matrix_distance[old_possible[old]+1][old_possible[dest]+1]
+  for o in range(len(possible)-1):
+    d = o + 1
+    distanceNew += matrix_distance[possible[o]+1][possible[d]+1]
+  distance = distanceNew - distanceOld
+  return distance
+
+
+def selectWorstPacket(route, md):
+    """
+    Selecionar o id do pior pacote da rota CONSIDERE O DEPOSITO
+    """
+    worstScore = 0
+    id_worst = -1
+    if len(route) > 0:
+        for i in range(1,len(route)):
+            score = availablePack(route, i, md)
+            if worstScore < score:
+                id_worst = route[i]
+    return id_worst
+
+def bestLocalTwoOptModificated(route1, route2, md, instance):
+    """Ajusta as melhores posições para as rotas"""
+    routes = [route1, route2]
+    bestScore = calculateDistanceRoutes(routes, md)+1
+    score = bestScore-1
+    while score < bestScore:
+        bestScore = score
+        score = twoOptStarModificatedScore(route1, route2, md, instance) 
+    return route1, route2
+
+def createNewRoute(packet_id, vehiclesPossibles, newCap, deliveries):
+    """Criar uma nova rota do deposito até o packet"""
+    try:
+        newVehicle = max(vehiclesPossibles) + 1
+    except ValueError:
+        newVehicle = 1
+    vehiclesPossibles[newVehicle] = [[0, packet_id], newCap]
+    deliveries.append(packet_id)
+    return vehiclesPossibles[newVehicle][0], newVehicle
+
+def lookForNeighboringRoutes(packet: Delivery, deliveries, md, vPoss: dict, T: int):
+    """Procura pelas rotas vizinhas"""
+    routes_neigs = []
+    neighs = {}
+    packs_neigs = []
+    for i in deliveries:
+      neighs[i] = md[packet.idu+1][i+1]
+    if len(deliveries) != 0:
+        for id in sorted(neighs, key = neighs.get):
+            packs_neigs.append(id)
+        auxT = 0
+        for d in packs_neigs:
+            if auxT < T:
+                for k, v in vPoss.items():
+                    if d in v:
+                        if k not in routes_neigs:
+                            routes_neigs.append(k)
+                        auxT += 1
+    return routes_neigs
 
 def solve_instance(
     instance: CVRPInstance
