ef0a7bf8f3ef8eeb09de2ae5b890bda74f1dede8
Wilton <dev3@evop.com.br>
2022-11-04 16:23:57 -0400

    Rodando!

--- loggibud/v1/baselines/task2/dsnr.py
+++ loggibud/v1/baselines/task2/dsnr.py
@@ -21,6 +21,7 @@ from loggibud.v1.types import (
     CVRPInstance,
     CVRPSolution,
     CVRPSolutionVehicle,
+    RoutePossible
 )
 
 logger = logging.getLogger(__name__)
@@ -55,7 +56,6 @@ def distributionBatch(
             deliveries
         )
         solution = buildSolution(instance, vehicles_possibles)
-        NUM_LOTE += 1
     return solution
 
 def buildSolution(instance: CVRPInstance, vehicles_occupation):
@@ -137,11 +137,12 @@ def routingBatches(
     reallocate = []
     while len(order_batch) > 0:
         poss = [p.idu for p in order_batch]
-        pack = order_batch.popleft()
+        pack = order_batch.pop(0)
         routePackage(
             vehicles_possibles,
             pack,
             matrix,
+            instance,
             order_batch,
             k_near,
             deliveries,
@@ -181,7 +182,7 @@ def buildMetric(instance, batch, packetsExist, order, md):
     for i in sorted(distances_pack, key = distances_pack.get, reverse=order):
         orderBatchList.append(instance.deliveries[i])
     orderBatch = deque(orderBatchList)
-    return 
+    return orderBatch
 
 def meanDistance(p, packetsExist, md):
     d = 0
@@ -331,6 +332,21 @@ def calculateDiferenceDistanceRoute(
   distance = distanceNew - distanceOld
   return distance
 
+def availablePack(route, i, md):
+    """
+    Avaliar o score de um pacote dentro de uma rota
+    """
+    if i >= 1: # a rota tem q ter pelo menos um pacote
+        id_prev = route[i-1]+1
+        id_act = route[i]+1
+        if i == (len(route)-1): # ultimo elemento
+            # id do prox pack = route[i-1] | route[i-1]+1 ?
+            return md[id_prev][id_act]
+        else:
+            id_next = route[i+1]
+            return md[id_prev][id_act] + md[id_act][id_next] - md[id_prev][id_next]
+    else:
+        return 0 
 
 def selectWorstPacket(route, md):
     """
@@ -354,6 +370,225 @@ def bestLocalTwoOptModificated(route1, route2, md, instance):
         bestScore = score
         score = twoOptStarModificatedScore(route1, route2, md, instance) 
     return route1, route2
+
+def twoOptStarModificatedScore(route1, route2, md, instance):
+    p1, p2 = 1, 1
+    routes = [route1, route2]
+    while p1 < len(route1) and p2 < len(route2):
+        # print(p1,p2)
+        possibles_solutions = constructPossiblesSolutions(
+            md, instance, 
+            route1, route2, 
+            p1, p2)
+        best_solution = selectPossible(possibles_solutions)
+        if best_solution != None:
+            route1, route2  = best_solution.route1.copy(), best_solution.route2.copy()
+            if best_solution.select[0] != best_solution.select[1]:
+                p1 += 1
+                p2 += 1
+            else:
+                if best_solution.select[0] == 1:
+                    p1 += 1
+                else:
+                    p2 += 1
+        else:
+            p1 += 1
+            p2 += 1
+    return calculateDistanceRoutes(routes, md)
+
+def selectPossible(possibles: List[RoutePossible]):
+    selected = None
+    value_s = 0
+    for possible in possibles:
+        if possible.gain > value_s:
+            selected = possible
+            value_s = possible.gain
+    return selected
+
+def compensationRemoveInRoute(
+    md, 
+    ro, 
+    i_remove # index -> ro[i_remove] = 152 -> 153 
+):
+    lose = 0 # ... calcular as arestas perdidas
+    gain = 0 # ... calcular as arestas adicionadas
+    if i_remove < (len(ro)-1): # ultimo elemento não tem perda
+        if i_remove == 1:
+            gain += md[ro[i_remove-1]][ro[i_remove+1]+1]
+            lose += md[ro[i_remove-1]][ro[i_remove]+1]
+            lose += md[ro[i_remove]+1][ro[i_remove+1]+1]
+        else:
+            gain += md[ro[i_remove-1]+1][ro[i_remove+1]+1]
+            lose += md[ro[i_remove-1]+1][ro[i_remove]+1]
+            lose += md[ro[i_remove]+1][ro[i_remove+1]+1]
+    else:
+        if i_remove == 1:
+            lose += md[ro[i_remove-1]][ro[i_remove]+1]
+        else:
+            lose += md[ro[i_remove-1]+1][ro[i_remove]+1]
+    return round((lose - gain)/1_000,3)
+
+def compensationInsertInRoute(
+    md,
+    rd,
+    p_insert, # 152 -> 153 
+    i_destiny # index = 3 
+    ):
+    gainDistanceTotal = 0
+    lose = 0
+    gain = 0
+    if i_destiny < (len(rd)-1): # ultimo elemento não tem perda
+        if i_destiny == 1:
+            lose += md[rd[i_destiny-1]][rd[i_destiny]+1]
+            gain += md[rd[i_destiny-1]][p_insert+1]
+            gain += md[p_insert+1][rd[i_destiny]+1]
+        else:
+            lose += md[rd[i_destiny-1]+1][rd[i_destiny]+1]
+            gain += md[rd[i_destiny-1]+1][p_insert+1]
+            gain += md[p_insert+1][rd[i_destiny]+1]
+    else:
+        if i_destiny == 1:
+            gain += md[rd[i_destiny-1]][p_insert+1]
+        else:
+            gain += md[rd[i_destiny-1]+1][p_insert+1]
+    gainDistanceTotal = round((lose - gain)/1_000,3)
+    return gainDistanceTotal
+
+def insertBothInRoute(
+    md, instance,
+    r1, r2, 
+    i_insert, selected: int):
+    removePack = compensationRemoveInRoute(md, r2, i_insert)
+    possibles = []
+    select = [selected, selected]
+    if constraintCapacityAdd(instance, r1, instance.deliveries[r2[i_insert]]):
+        for i in range(1,len(r1)+1):
+            poss = {}
+            new_route1 = r1.copy()
+            new_route1.insert(i, r2[i_insert])
+            gain = compensationInsertInRoute(md, r1, r2[i_insert], i)
+            new_route2 = [cor for cor in r2 if cor != r2[i_insert]] # remover o r2[p_insert]
+            if gain > 0:
+                poss = RoutePossible(new_route1, new_route2, round((gain+removePack),3), select)
+                possibles.append(poss)
+        possibleInsert = selectPossible(possibles)
+    else:
+        return None # nao ocupou espaço
+    return possibleInsert
+
+def constraintCapacityAdd(instance, route, arrive: Delivery):
+    CAP = instance.vehicle_capacity
+    DEP = 0
+    total = 0 
+    for i in route:
+        if DEP == 0:
+            DEP += 1
+        else:
+            total += instance.deliveries[i].size
+    packSize = arrive.size
+    return total + packSize <= CAP
+
+def constructPossiblesSolutions(
+    md, instance: CVRPInstance, 
+    route1: list, route2: list, 
+    p1: int, p2: int):
+    possibles = []
+    swap = swapPacks(md, instance, route1, route2, p1, p2)
+    if swap != None:
+        possibles.append(swap)
+    possibleRoute1 = insertBothInRoute(md, instance, route1, route2, p2, 1) # p2 -> route1
+    if possibleRoute1 != None:
+        possibles.append(possibleRoute1)
+    possibleRoute2 = insertBothInRoute(md, instance, route2, route1, p1, 2) # p1 -> route2
+    if possibleRoute2 != None:
+        possibles.append(possibleRoute2)
+    return possibles
+
+def calculateDistanceRoutes(routes, matrix_distance):
+  """Calcular a distancia percorrida por uma lista de rota"""
+  distance = []
+  for route in routes:
+    distance.append(computeDistanceRoute(route, matrix_distance))
+  return sum(distance)
+
+def swapPossible(instance, route1, route2, p1, p2):
+    # somar todas as cargas da rota1 - packet1 + packet2
+    return constraintCapacity(
+        instance, 
+        route1, p1, instance.deliveries[route2[p2]]) and constraintCapacity(
+            instance, 
+            route2, p2, instance.deliveries[route1[p1]])
+
+def constraintCapacity(instance, route, left, arrive: Delivery):
+    CAP = instance.vehicle_capacity
+    DEP = 0
+    total = 0 
+    for i in route:
+        if DEP == 0:
+            DEP += 1
+        else:
+            total += instance.deliveries[i].size
+    sizeLeft = instance.deliveries[route[left]].size
+    packSize = arrive.size
+    return total - sizeLeft + packSize <= CAP
+
+def compensationSwap(md, r1, r2, i, j):
+    if i != (len(r1)-1):
+        if i-1 == 0:
+            gain = md[r1[i-1]][r2[j]+1] + md[r2[j]+1][r1[i+1]+1]
+            lose = md[r1[i-1]][r1[i]+1] + md[r1[i]+1][r1[i+1]+1]
+        else:
+            gain = md[r1[i-1]+1][r2[j]+1] + md[r2[j]+1][r1[i+1]+1]
+            lose = md[r1[i-1]+1][r1[i]+1] + md[r1[i]+1][r1[i+1]+1]
+    else:
+        if i-1 == 0:
+            gain = md[r1[i-1]][r2[j]+1]
+            lose = md[r1[i-1]][r1[i]+1]
+        else:
+            gain = md[r1[i-1]+1][r2[j]+1]
+            lose = md[r1[i-1]+1][r1[i]+1]
+    return gain, lose
+
+def computeCompensationSwap(md, route1, route2, p1, p2):
+    gain1, lose1 = compensationSwap(md, route1, route2, p1, p2)
+    gain2, lose2 = compensationSwap(md, route2, route1, p2, p1)
+    loseSwap = lose1 + lose2
+    gainSwap = gain1 + gain2
+    return round((gainSwap)/1_000,3), round((loseSwap)/1_000, 3)
+
+def swapPacks(
+    matrix_distance, 
+    instance: CVRPInstance, 
+    route1: list, route2: list, 
+    p1: int, p2: int) -> RoutePossible:
+    if swapPossible(instance, route1, route2, p1, p2):
+        select = [2, 1]
+        gainSwap, loseSwap = computeCompensationSwap(
+                    matrix_distance, route1, route2, p1, p2)
+        if loseSwap > gainSwap:
+            new_route1 = [
+                route1[i] 
+                for i in range(len(route1)) 
+                if i != p1
+            ]
+            new_route1.insert(p1, route2[p2])
+            new_route2 = [
+                route2[j] 
+                for j in range(len(route2)) 
+                if j != p2
+            ]
+            new_route2.insert(p2, route1[p1])
+            gain = round((loseSwap - gainSwap), 3)
+            possibleSwap = RoutePossible(
+                new_route1,
+                new_route2,
+                gain,
+                select)
+        else:
+            return None # lose < gain
+    else:
+        return None # nao respeita capacidade
+    return possibleSwap
 
 def createNewRoute(packet_id, vehiclesPossibles, newCap, deliveries):
     """Criar uma nova rota do deposito até o packet"""
@@ -405,7 +640,7 @@ if __name__ == "__main__":
     parser.add_argument("--output", type=str)
     parser.add_argument("--params", type=str)
     args = parser.parse_args()
-
+    args.organization = None
     # Load instance and heuristic params.
     eval_path = Path(args.eval_instances)
     eval_path_dir = eval_path if eval_path.is_dir() else eval_path.parent
@@ -435,19 +670,20 @@ if __name__ == "__main__":
         solution = distributionBatch(
             instance, 
             matrix_distance, 
-            k_near = args.k_near, 
-            batch_size = args.batch_size, 
+            k_near = int(args.k_near), 
+            batch_size = int(args.batch_size), 
             organization=args.organization
         )
 
-        logger.info("Starting to dynamic route.")
-        for delivery in tqdm(instance.deliveries):
-            model_finetuned = route(model_finetuned, delivery)
+        # logger.info("Starting to dynamic route.")
+        # for delivery in tqdm(instance.deliveries):
+        #     model_finetuned = route(model_finetuned, delivery)
 
-        solution = finish(instance, model_finetuned)
+        # solution = finish(instance, model_finetuned)
 
         solution.to_file(output_dir / f"{instance.name}.json")
-
+    print(eval_path)
+    solve(eval_path)
     # Run solver on multiprocessing pool.
-    with Pool(os.cpu_count()) as pool:
-        list(tqdm(pool.imap(solve, eval_files), total=len(eval_files)))
+    # with Pool(os.cpu_count()) as pool:
+    #     list(tqdm(pool.imap(solve, eval_files), total=len(eval_files)))
